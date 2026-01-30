221148c2ed8778ae7afa4c9075a97a5d9ef0941e
MarkDR <97461255+imneil04@users.noreply.github.com>
2026-01-30 10:50:14 -0700

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './context/CartContext.jsx'
+import { HashRouter } from 'react-router-dom' //used for now since BrowserRouter is not fully supported when deploying in gh pages
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
